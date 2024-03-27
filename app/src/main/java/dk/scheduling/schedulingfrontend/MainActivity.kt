49d23245dd0f40228d843124aa047dfedbc7d935
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2024-03-27 11:39:44 +0100

    Update gitignore

--- app/src/main/java/dk/scheduling/schedulingfrontend/MainActivity.kt
+++ app/src/main/java/dk/scheduling/schedulingfrontend/MainActivity.kt
@@ -2,6 +2,10 @@ package dk.scheduling.schedulingfrontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,7 +14,9 @@ import androidx.compose.ui.tooling.preview.Preview
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Greeting("Android")
+        }
     }
 }
 
