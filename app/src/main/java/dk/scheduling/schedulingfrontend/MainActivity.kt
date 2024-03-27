e718cf42ccbe23f0594994cb28a5873edb7f22c4
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2024-03-27 11:29:05 +0100

    Update MainActivity.kt

--- app/src/main/java/dk/scheduling/schedulingfrontend/MainActivity.kt
+++ app/src/main/java/dk/scheduling/schedulingfrontend/MainActivity.kt
@@ -2,10 +2,28 @@ package dk.scheduling.schedulingfrontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Greeting("Android")
 }
