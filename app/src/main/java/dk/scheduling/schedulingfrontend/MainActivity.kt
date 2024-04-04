886f4cc549ca2d13a00df75e731fcd9ce509d3a7
henneboy <72788863+henneboy@users.noreply.github.com>
2024-04-04 13:16:15 +0200

    Update MainActivity.kt

--- app/src/main/java/dk/scheduling/schedulingfrontend/MainActivity.kt
+++ app/src/main/java/dk/scheduling/schedulingfrontend/MainActivity.kt
@@ -4,16 +4,13 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
 import dk.scheduling.schedulingfrontend.ui.theme.SchedulingFrontendTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SchedulingFrontendTheme {
-                // ApiButton()
                 App()
             }
         }
